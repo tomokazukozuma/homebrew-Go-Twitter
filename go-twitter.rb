40127b819ad0755996e418119b64ba7c6077ac86
Tomokazu Kozuma <tomokazu.kozuma@gmail.com>
2018-11-25 17:15:36 +0900

    Remove homepage

--- go-twitter.rb
+++ go-twitter.rb
@@ -3,7 +3,6 @@
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class GoTwitter < Formula
   desc "Search and Get Tweet by CLI"
-  homepage ""
   url "https://github.com/tomokazukozuma/Go-Twitter/releases/download/v0.0.2/go-twitter"
   sha256 "3b5de7eddcf5dbc45d092f61bd3f1fc208c761cd448aab7a0c3e4d310c961d51"
   # depends_on "cmake" => :build
